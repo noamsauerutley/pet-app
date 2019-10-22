b53a634742dff6fd812811b7d6ec0b6228f48b70
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-10-22 16:59:49 -0400

    Created root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :shelters, only: [:new,:create,:show,:delete]
   resources :adopters 
 
+  root 'adopters#index'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
