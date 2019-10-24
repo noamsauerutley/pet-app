e4e6cfd27fb8cd5056e358ef8830e221a75d5248
solomonp90 <solomonraulpena@gmail.com>
2019-10-24 12:20:21 -0400

    made adopted pets clickable

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  # delete "/logout", to: "sessions#destroy"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
